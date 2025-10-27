86cf4b4398650e4830663baa70a7fcb883719036
Pedro Miguel Malheiro Gonçalves <135701205+FireLegend03@users.noreply.github.com>
2025-10-27 14:44:49 +0000

    Fill map upgrade, highlights work now

--- Scripts/BaseMap.gd
+++ Scripts/BaseMap.gd
@@ -7,6 +7,7 @@ class_name BaseMap
 @export var width: int
 @export var height: int
 @onready var highlight_map: TileMap = $HighlightMap
+@onready var tile_map: TileMap = $TileMap
 
 const TILE_SIZE = 28
 
@@ -25,12 +26,26 @@ func _ready():
 	var cursor = get_node("Cursor")
 	cursor.cursor_moved.connect(_on_cursor_moved)
 	
+	# Fill the map grid based on the TileMap
+	fill_map_grid()
+
 	# Initialize unit grid with nulls
 	for x in range(width):
 		var column = []
 		for y in range(height):
 			column.append(null) 
 		unit_grid.append(column)
+	
+	
+
+# Fills the map matrix with tile resources based on the TileMap
+func fill_map_grid():
+	for x in range(width):
+		var column = []
+		for y in range(height):
+			var tile_type = tile_map.get_cell_tile_data(0, Vector2i(x, y)).get_custom_data("terrain")
+			column.append(tiles[tile_type])
+		map_grid.append(column)
 
 
 # Return the array with all walkable tiles for a given unit (to paint in blue)
@@ -43,7 +58,7 @@ func get_walkable_tiles(unit, pos: Vector2i) -> Array:
 		var remaining_movement = remaining_tile_movement[current_pos]
 
 		for neighbor in get_neighbors(current_pos):
-			var tile = map_grid[neighbor.y][neighbor.x]
+			var tile = map_grid[neighbor.x][neighbor.y]
 			var new_remaining_movement = tile.can_unit_enter(unit.get_type(), remaining_movement)
 
 			if (new_remaining_movement >= 0 and 
@@ -71,9 +86,9 @@ func get_neighbors(pos: Vector2i) -> Array:
 func _on_cursor_moved(new_position: Vector2) -> void:
 	var pos = Vector2i(new_position / TILE_SIZE)
 	print("Cursor moved to: ", pos) # For debugging purposes
+	print("tile: ", map_grid[pos.x][pos.y])
 
 	var unit = unit_grid[pos.x][pos.y]
-	print("Unit at cursor: ", unit) # For debugging purposes
 
 	if unit:
 		var walkable_tiles = get_walkable_tiles(unit, pos)
@@ -84,4 +99,4 @@ func _on_cursor_moved(new_position: Vector2) -> void:
 func highlight_tiles(walkable_tiles: Array) -> void:
 	highlight_map.clear()
 	for tile in walkable_tiles:
-		highlight_map.set_cell(0, tile, 1)  # Set the tile to a highlighted state
+		highlight_map.set_cell(0, tile, 0, Vector2i(0,0))  # Set the tile to a highlighted state
